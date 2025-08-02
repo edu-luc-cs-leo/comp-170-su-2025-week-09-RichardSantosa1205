cc25604a7f79c752b57059aa3a79fad671a54eb0
Richard Zhang Santosa <rsantosa@luc.edu>
2025-08-02 15:28:48 +0000

    just need reflections

--- week09.py
+++ week09.py
@@ -50,3 +50,122 @@ def count_occurrences_iterative(data_list, target):
 
 # WRITE YOUR CODE BELOW
 
+def sum_of_digits(n):
+    """
+    Recursively returns the sum of the digits of n
+    """
+    # Handle negatives
+    n = abs(n)
+
+    # Store result in this variable
+    result = 0
+
+    if n < 10:
+        # This is base case
+        result = n
+    else:
+        # Grab the last digit, then perform recursion on the rest of the numbers.
+
+        last_digit = n % 10
+        remaining = n // 10
+        result = last_digit + sum_of_digits(remaining)
+
+    # Return the result of the recursion
+    return result
+
+print("sum_of_digits:")
+print(sum_of_digits(789))
+print(sum_of_digits(123))
+print(sum_of_digits(0))
+print(sum_of_digits(9876654))
+
+def count_occurrences(data_list, target):
+    """
+    Recursively counts how many times target appears in data_list.
+    """
+
+    # Store result here
+    result = 0
+
+    if len(data_list) == 0:
+        # This is base case
+        result = 0
+    else:
+        # Grab the first item, and then pass in the rest of the items to the next iteration of the recursion.
+        first_item = data_list[0]
+        rest_of_list = data_list[1:]
+
+        # If an occurence is found, add one.
+        if first_item == target:
+
+            result = 1 + count_occurrences(rest_of_list, target)
+        else:
+            result = count_occurrences(rest_of_list, target)
+
+    return result
+
+print("count_occurrences:")
+print(count_occurrences([1,2,3,4,4,5,6,7,8,1,1,1,2,2,3], 1))
+print(count_occurrences([1,2], 1))
+print(count_occurrences([], 1))
+
+def factorial_iterative(n):
+    """
+    Computes n! using an iterative loop.
+    """
+    
+    # Store the result in this variable
+    result = 1 
+
+    # Since result is always 1, we can start multiplying from 2 until n.
+    # Range is until n+1 because we want to include n
+    for i in range(2, n + 1):
+        result *= i
+
+    return result
+
+print("factorial_iterative:")
+print(factorial_iterative(5))
+print(factorial_iterative(2))
+print(factorial_iterative(1))
+print(factorial_iterative(10))
+
+
+def fibonacci_iterative(n):
+    """
+    Computes F(n) iteratively where F(1)=1, F(2)=2, and F(n)=F(n-1)+F(n-2).
+    """
+
+    # Store the results in this variable
+    result = 0
+
+    if n <= 0:
+        # Check if n is a valid number
+        result = 0
+    elif n == 1 or n == 2:
+        # If n is either 1 or 2, just set the result to be 1 or 2
+        result = n
+    else:
+        prev_two = 1 # this will be F(1)
+        prev_one = 2 # this will be F(2)
+
+        # Since we already know the values of F(1) and F(2), we can start from index 3
+        i = 3
+
+        # Now perform the Fibonacci sequence until F(n) is achieved.
+        while i <= n:
+            current = prev_one + prev_two
+            prev_two = prev_one
+            prev_one = current
+            i = i + 1
+
+        # Store the value which will be returned at the end.
+        result = prev_one
+
+    return result
+
+print("fibonacci_iterative:")
+print(fibonacci_iterative(5))
+print(fibonacci_iterative(6))
+print(fibonacci_iterative(7))
+print(fibonacci_iterative(8))
